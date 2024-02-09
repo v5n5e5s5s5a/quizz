cad1633b7e626ef8687995f32d5b5957392f7a8f
v5n5e5s5s5a <105273675+v5n5e5s5s5a@users.noreply.github.com>
2024-02-09 22:30:43 +0300

    quizz

---
+++ quizz.js
@@ -0,0 +1,49 @@
+import readline from 'readline'
+import fs from 'fs'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let quizz = []
+
+try {
+    const info = fs.readFileSync('questions.json', 'utf-8')
+    quizz = JSON.parse(info)
+} catch (error) {
+    console.log('error reading questions:', error.message)
+    process.exit(1)
+}
+
+const generateQuestion = (index, score) => {
+    if (index === quizz.length) {
+        rl.close();
+        console.log(`Your final score is ${score} out of ${quizz.length}`)
+        return;
+    }
+
+    const CQ = quizz[index];
+
+    rl.question(` ${index + 1}. ${CQ.question}\n${CQ.answers.join('\n')}\n Enter Answer: `, (inputAnswer) => {
+        const userAnswer = parseInt(inputAnswer) - 1
+
+        if (userAnswer >= 0 && userAnswer < CQ.answers.length) {
+            const trueAnswer = CQ.correctAnswerIndex === userAnswer
+            if (trueAnswer) {
+                console.log('Answer is Correct\n')
+                score ++
+            } else {
+                console.log(`Your answer is wrong! ${CQ.answers[CQ.correctAnswerIndex]} \n`);
+            }
+
+            generateQuestion(index + 1, score)
+        } else {
+            console.log('Invalid answer! Enter a number which corresponds to the answer of your choice \n');
+            generateQuestion(index, score)
+        }
+    })
+}
+
+console.log(`QUIZZ \n Select the correct answer for the following Questions \n`);
+generateQuestion(0, 0)
